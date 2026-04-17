e0800689d3b71f2d8c16d9efab0353701209f34c
yjlu12 <yjlu12@iflytek.com>
2026-04-17 10:38:04 +0800

    feat: Add workflow export as template feature

--- console/backend/hub/src/main/java/com/iflytek/astron/console/hub/entity/maas/MaasTemplate.java
+++ console/backend/hub/src/main/java/com/iflytek/astron/console/hub/entity/maas/MaasTemplate.java
@@ -2,6 +2,8 @@ package com.iflytek.astron.console.hub.entity.maas;
 
 import com.alibaba.fastjson2.JSONObject;
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import lombok.Data;
 import org.springframework.format.annotation.DateTimeFormat;
 
@@ -13,19 +15,23 @@ public class MaasTemplate {
     public static final String TEMPLATE_SOURCE_OFFICIAL = "OFFICIAL";
     public static final String TEMPLATE_SOURCE_EXPORTED = "EXPORTED";
 
+    @JsonSerialize(using = ToStringSerializer.class)
     private Long id;
     private JSONObject coreAbilities;
     private JSONObject coreScenarios;
     private Byte isAct;
+    @JsonSerialize(using = ToStringSerializer.class)
     private Long maasId;
     private String subtitle;
     private String title;
     private Integer botId;
     private String coverUrl;
+    @JsonSerialize(using = ToStringSerializer.class)
     private Long groupId;
     private String groupName;
     private String groupNameEn;
     private Integer orderIndex;
+    @JsonSerialize(using = ToStringSerializer.class)
     private Long templateId;
     private String templateSource;
     private Boolean deletable;
