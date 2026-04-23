6eab84736899f181303792339e03a84e4661725a
yjlu12 <yjlu12@iflytek.com>
2026-04-23 14:20:01 +0800

    feat: Added skill system

---
+++ console/backend/toolkit/src/main/java/com/iflytek/astron/console/toolkit/entity/dto/skill/SkillDirectoryUploadResultDto.java
@@ -0,0 +1,11 @@
+package com.iflytek.astron.console.toolkit.entity.dto.skill;
+
+import java.util.ArrayList;
+import java.util.List;
+import lombok.Data;
+
+@Data
+public class SkillDirectoryUploadResultDto {
+    private List<SkillFileTreeNodeDto> tree = new ArrayList<>();
+    private List<SkillFileTreeNodeDto> uploadedNodes = new ArrayList<>();
+}
